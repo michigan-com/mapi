4cbf9c750cb14c5942bfe7e11aa05e25110cedd8
Eric Bower <neurosnap@gmail.com>
2015-07-15 13:17:18 -0400

    some testing stuff

--- src/tests/get/news.js
+++ src/tests/get/news.js
@@ -1,22 +1,36 @@
 import mocha from 'mocha';
 import { assert } from 'chai';
 
+import app from '../../app';
 import { getNewsArticles } from '../../get/news';
-import { connect, disconnect } from '../../db';
+import { Article, connect, disconnect } from '../../db';
 import { testDb } from '../../../config';
 import logger from '../../logger';
 
-describe('News fetching', function() {
+describe('News fetching and saving', function() {
   before(function(done) {
-    connect(testDb).then(function() {
+    (async function() {
+      try {
+        await connect(testDb);
+      } catch (err) {
+        logger.error(err);
+      }
+
+      getNewsArticles(app).catch(function(err) { logger.error(err); });
       done();
-    });
+    })();
   });
 
   after(function(done) {
-    disconnect().then(function() {
+    (async function() {
+      try {
+        await Article.remove().exec();
+        await disconnect();
+      } catch (err) {
+        logger.error(err);
+      }
       done();
-    });
+    })();
   });
 
   it('Fetches articles and verifies the return values', async function(done) {
