75c5fa3a11de8b61136ac7c039ba0a82251be4ec
Eric Bower <neurosnap@gmail.com>
2015-07-15 12:43:46 -0400

    more log handling fixes

--- src/app.js
+++ src/app.js
@@ -39,8 +39,16 @@ app.use('/v1/', routes.v1);
 routes.socket.articles(app);
 
 app.use(function(err, req, res, next) {
-  if (apiErrorCodes.indexOf(err.status) == -1) logger.error(err);
-  next(err);
+  if (apiErrorCodes.indexOf(err.status) == -1) {
+    logger.error(err);
+    next(err);
+  } else {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  }
 });
 
 if (app_env === 'development' || app_env === 'testing') {
@@ -52,16 +60,6 @@ if (app_env === 'development' || app_env === 'testing') {
     });
   });
 }
-
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: err
-  });
-
-  next(err);
-});
 
 app.use(function(err, req, res, next) {
   mail.mailOptions.text = `
