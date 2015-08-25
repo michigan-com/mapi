a6a5f76132a784e17640d3a04972c8eadedc9b56
Eric Bower <neurosnap@gmail.com>
2015-08-25 14:11:36 -0400

    updated config defaults

--- config_default.js
+++ config_default.js
@@ -1,23 +1,7 @@
 'use strict';
 
-var path = require('path');
-var fs = require('fs');
-
 module.exports = {
   db: 'mongodb://localhost:27017/mapi',
-  log: {
-    appenders: [{
-      type: 'console'
-    }, {
-      type: 'file',
-      filename: path.join(__dirname, 'express.log'),
-      maxLogSize: 1000000,
-      backups: 3,
-      category: 'dev'
-    }]
-  },
-  //optional
-  log_level: 'INFO',
   //optional
   smtp: {
     service: 'gmail',
