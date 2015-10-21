3b42aebbf05c58d9643c2eb02f16f6f90277b9c6
Andrey Tarantsov <andrey@tarantsov.com>
2015-10-21 22:03:50 +0600

    ✂️ Export raw collections from db

--- src/server/db.js
+++ src/server/db.js
@@ -6,11 +6,23 @@ var logger = debug('app:db');
 
 import { db } from '../config';
 
+export let mdb = {};
+export let ObjectId = mongoose.mongo.ObjectId;
+
+
 if (typeof db === 'undefined') {
   throw new Error("`db` key in config.js is required to connect to mongodb, ex: db: 'mongodb://localhost:27017/db'");
 }
 
 var Schema = mongoose.Schema;
+
+export let Recipe = mongoose.model('Recipe', new Schema({}), 'Recipe');
+export let Article = mongoose.model('Article', new Schema({}), 'Article');
+export let Toppages = mongoose.model('Toppages', new Schema({}), 'Toppages');
+export let Quickstats = mongoose.model('Quickstats', new Schema({}), 'Quickstats');
+export let Topgeo = mongoose.model('Topgeo', new Schema({}), 'Topgeo');
+export let Referrers = mongoose.model('Referrers', new Schema({}), 'Referrers');
+
 
 const defaults = {
   server: {
@@ -18,7 +30,7 @@ const defaults = {
   }
 };
 
-function connect(dbString, options=defaults) {
+export function connect(dbString, options=defaults) {
   if (!dbString) throw new Error('dbString must be defined');
 
   logger(`Connecting to: ${dbString}`);
@@ -26,12 +38,13 @@ function connect(dbString, options=defaults) {
     mongoose.connect(dbString, options, function(err) {
       if (err) reject(err);
       logger('Connected to mongodb!');
+      setupCollections();
       resolve(true);
     });
   });
 }
 
-function disconnect() {
+export function disconnect() {
   logger('Disconnecting from mongodb ...');
   return new Promise(function(resolve, reject) {
     mongoose.disconnect(function(err) {
@@ -42,14 +55,6 @@ function disconnect() {
   });
 }
 
-module.exports = {
-  Article: mongoose.model('Article', new Schema({}), 'Article'),
-  Recipe: mongoose.model('Recipe', new Schema({}), 'Recipe'),
-  Toppages: mongoose.model('Toppages', new Schema({}), 'Toppages'),
-  Quickstats: mongoose.model('Quickstats', new Schema({}), 'Quickstats'),
-  Topgeo: mongoose.model('Topgeo', new Schema({}), 'Topgeo'),
-  Referrers: mongoose.model('Referrers', new Schema({}), 'Referrers'),
-  connect,
-  disconnect
-};
-
+function setupCollections() {
+  mdb.recipes = Recipe.collection;
+}
